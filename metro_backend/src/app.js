28509497f518c1ca839ed3060bc0d36e46c67ed6
Saksham Panghal <saksham.panghaal@gmail.com>
2026-03-14 12:04:56 +0530

    Add cookie-parser dependency and update app configuration
    - Added cookie-parser to package.json and package-lock.json
    - Updated app.js to include cookie-parser middleware for handling cookies
    - Modified server.js to restore server listening functionality
    - Adjusted CORS configuration in app.js

--- metro_backend/src/app.js
+++ metro_backend/src/app.js
@@ -7,17 +7,20 @@ const app = express();
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    // origin: process.env.CORS_ORIGIN,
     credentials: true,
 })); //used for middleware and handling CORS errors
 
-app.use(express.json({limit: "16kb"})); //used for parsing application/json
+app.use(express.json()); //used for parsing application/json
+// app.use(express.json({limit: "16kb"})); //used for parsing application/json
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //used for parsing application/x-www-form-urlencoded
 app.use(express.static('public')); //used for serving static files
 app.use(cookieParser()); //used for parsing cookies
 
 // routes
-
+app.get("/", (req, res) => {
+    res.send("Welcome to Metro Routing System API");
+});
 
 //routes declaration
 
--- metro_backend/src/server.js
+++ metro_backend/src/server.js
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
+import app from './app.js';
 
 dotenv.config({
     path: './.env'
 });
 
 
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server is running on port ${process.env.PORT}`);
-// });
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
